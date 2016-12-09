128c9b5fe51f5bc4765e4d63df8dece1d160e6b5
John Deverall <johndeverall@gmail.com>
2016-12-09 01:04:57 +0100

    Update ClientCredentialsGrant.java

--- java-api-client-library/src/main/java/nz/co/thescene/client/auth/grants/ClientCredentialsGrant.java
+++ java-api-client-library/src/main/java/nz/co/thescene/client/auth/grants/ClientCredentialsGrant.java
@@ -1,17 +1,21 @@
-# Copyright (C) 2012-2016 TheScene.Co Ltd.
-# This file is part of TheScene.Co Java Client SDK.
-#
-# TheScene.Co Java Client SDK is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# TheScene.Co Java Client SDK is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
+/* 
+
+Copyright (C) 2012-2016 TheScene.Co Ltd.
+This file is part of TheScene.Co Java Client SDK.
+
+TheScene.Co Java Client SDK is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TheScene.Co Java Client SDK is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License.
+
+*/
 
 package nz.co.thescene.client.auth.grants;
 
