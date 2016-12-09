1ed49cac8a641bbba46fb9706dbdaf2a772e1af8
John Deverall <johndeverall@gmail.com>
2016-12-09 18:35:52 +0100

    Removing dependency on data generator temporarily

--- console/src/main/java/nz/co/thescene/console/menu/webadmin/DataGenerationMenu.java
+++ console/src/main/java/nz/co/thescene/console/menu/webadmin/DataGenerationMenu.java
@@ -1,6 +1,6 @@
 package nz.co.thescene.console.menu.webadmin;
 
-import nz.co.thescene.DataGenerator;
+//import nz.co.thescene.DataGenerator;
 import nz.co.thescene.client.errors.SceneClientException;
 import nz.co.thescene.console.menu.Menu;
 import nz.co.thescene.dto.json.hal.MemberResource;
@@ -22,8 +22,8 @@ public class DataGenerationMenu extends Menu {
         if (userInput.equals("1")) {
             String quantity = getUserInput("Please enter max quantity of events. Events generated with step by 20. ");
             int limit = Integer.parseInt(quantity);
-            DataGenerator dataGenerator = new DataGenerator();
-            dataGenerator.generateEvents(member, limit);
+            //DataGenerator dataGenerator = new DataGenerator();
+            //dataGenerator.generateEvents(member, limit);
         } else {
             handleInvalidInput();
         }
